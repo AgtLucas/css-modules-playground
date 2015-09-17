1a3c51938b4d4684b2caa474e062a7c0582a3c10
Lucas <lucas@agtlucas.com>
2015-09-17 09:00:38 -0300

    Update Logo.js

---
+++ src/components/0-Logo/Logo.js
@@ -0,0 +1,11 @@
+import styles from './Logo.css';
+
+import React from 'react';
+
+export default class Logo extends React.Component {
+
+  render() {
+    return <div className={styles.logo} />;
+  }
+
+};
