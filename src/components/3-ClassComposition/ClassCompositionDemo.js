d02238123f7632e7af3b119370219da985cb6a6a
Lucas <lucas@agtlucas.com>
2015-09-18 09:07:23 -0300

    Update ClassCompositionDemo.js

---
+++ src/components/3-ClassComposition/ClassCompositionDemo.js
@@ -0,0 +1,29 @@
+import ClassComposition from './ClassComposition';
+
+import React from 'react';
+
+import js from '!!raw!./StyleVariantA/StyleVariantA.js';
+import css from '!!raw!./StyleVariantA/StyleVariantA.css';
+import layoutCss from '!!raw!shared/styles/layout.css';
+import typographyCss from '!!raw!shared/styles/typography.css';
+import Snippet from 'shared/Snippet/Snippet';
+
+export default class ClassCompositionDemo extends React.Component {
+
+  render() {
+
+    const files = [
+      { name: 'StyleVariantA.js', source: js },
+      { name: 'StyleVariantA.css', source: css },
+      { name: 'shared/styles/layout.css', source: layoutCss },
+      { name: 'shared/styles/typography.css', source: typographyCss }
+    ];
+
+    return(
+      <Snippet files={files}>
+        <ClassComposition />
+      </Snippet>
+    )
+  }
+
+};
