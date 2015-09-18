40a08557717faa75c4e7454ab87642a8c2081c9b
Lucas <lucas@agtlucas.com>
2015-09-18 08:42:32 -0300

    Update GlobalSelectorsDemo.js

---
+++ src/components/2-GlobalSelectors/GlobalSelectorsDemo.js
@@ -0,0 +1,25 @@
+import GlobalSelectors from './GlobalSelectors.css';
+
+import React from 'react';
+
+import js from '!!raw!./GlobalSelectors.js';
+import css from '!!raw!./GlobalSelectors.css';
+import Snippet from 'shared/Snippet/Snippet';
+
+export default class GlobalSelectorsDemo extends React.Component {
+
+  render() {
+    const files = [
+      { name: 'GlobalSelectors.js', source: js },
+      { name: 'GlobalSelectors.css', source: css }
+    ];
+
+    return(
+      <Snippet files={files}>
+        <GlobalSelectors />
+      </Snippet>
+    );
+
+  }
+
+};
