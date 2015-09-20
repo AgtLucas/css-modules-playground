c5293560648857b67fdb44ae1bc19c46bc47ff1f
Lucas <lucas@agtlucas.com>
2015-09-20 08:42:22 -0300

    Fix another typos

--- src/components/4-CompositionOverrides/CompositionOverridesDemo.js
+++ src/components/4-CompositionOverrides/CompositionOverridesDemo.js
@@ -2,25 +2,25 @@ import CompositionOverrides from './CompositionOverrides';
 
 import React from 'react';
 
-import js from '!!raw!./CompostionOverrides.js';
-import css from '!!raw!./CompostionOverrides.css';
+import js from '!!raw!./CompositionOverrides.js';
+import css from '!!raw!./CompositionOverrides.css';
 import layoutCss from '!!raw!shared/styles/layout.css';
 import typographyCss from '!!raw!shared/styles/typography.css';
 import Snippet from 'shared/Snippet/Snippet';
 
-export default class CompostionOverridesDemo extends React.Component {
+export default class CompositionOverridesDemo extends React.Component {
 
   render() {
     const files = [
-      { name: 'CompostionOverrides.js', source: js },
-      { name: 'CompostionOverrides.css', source: css },
+      { name: 'CompositionOverrides.js', source: js },
+      { name: 'CompositionOverrides.css', source: css },
       { name: 'shared/styles/layout.css', source: layoutCss },
       { name: 'shared/styles/typography.css', source: typographyCss }
     ];
 
     return(
       <Snippet files={files}>
-        <CompostionOverrides />
+        <CompositionOverrides />
       </Snippet>
     );
 
